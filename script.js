fc680e0dd75389862acee00dc9e8dba923008d56
Rider2ja <rider3ja@gmail.com>
2026-07-23 08:44:04 +0200

    Refactor profile management and like/dislike functions

--- script.js
+++ script.js
@@ -1,7 +1,74 @@
-function like() {
-    alert("❤️ Like gesendet!");
+let profile = 0;
+
+
+let users = [
+
+{
+name:"Sophie, 19",
+img:"https://i.pravatar.cc/300?1",
+info:"🎵 Musik 🎮 Gaming 🌍 Reisen"
+},
+
+{
+name:"Max, 20",
+img:"https://i.pravatar.cc/300?2",
+info:"🏀 Basketball 🎬 Filme"
+},
+
+{
+name:"Lena, 18",
+img:"https://i.pravatar.cc/300?3",
+info:"📚 Lesen 🎨 Kunst"
 }
 
-function skip() {
-    alert("➡️ Nächstes Profil");
+];
+
+
+function showProfile(){
+
+document.getElementById("name").innerHTML =
+users[profile].name;
+
+
+document.getElementById("bild").src =
+users[profile].img;
+
+
+document.getElementById("info").innerHTML =
+users[profile].info;
+
+}
+
+
+
+function like(){
+
+alert("❤️ Like gesendet!");
+
+next();
+
+}
+
+
+function dislike(){
+
+alert("Nächstes Profil");
+
+next();
+
+}
+
+
+function next(){
+
+profile++;
+
+if(profile >= users.length){
+
+profile=0;
+
+}
+
+showProfile();
+
 }
